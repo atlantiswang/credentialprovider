1db63e4547f179454229535262448e12f2d7d164
wanghao <atlantiswang@foxmail.com>
2017-08-24 18:55:26 +0800

    1. 提交未能删除的文件

--- SSCredProvider/SSCredProvider/SSCredentialInterface.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class ISSCredentialProviderCredential : public ICredentialProviderCredential
-{
-public:
-	virtual HRESULT Initialize(CREDENTIAL_PROVIDER_USAGE_SCENARIO cpus, DWORD cpusFlags,
-		ICredentialProvider *pCredentialProvider) = 0;
-};
